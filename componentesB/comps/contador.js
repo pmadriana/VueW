36adc2847ec76985ed8be8d79f25193586534484
Adriana Perez <adriana.perez@ucsp.edu.pe>
2021-03-10 13:47:18 -0500

    componentes basicos

---
+++ componentesB/comps/contador.js
@@ -0,0 +1,16 @@
+Vue.component('contador',{
+    template://html
+    `
+    <div>
+        <h3> {{numero}} </h3>
+        <button @click="numero++">+ </button>
+    </div>
+    `,
+    data(){
+        return {
+            numero: 0
+        }
+    }
+    
+
+});
---
+++ componentesB/comps/saludos.js
@@ -0,0 +1,16 @@
+Vue.component('saludo',{
+    template://html
+    `
+    <div>
+        <h1> {{saludo}}</h1>
+        <h3> dfmskfms </h3>
+    </div>
+    `,
+    data(){
+        return {
+            saludo: 'hola desde Vue'
+        }
+    }
+    
+
+});
